bb316dc87c73bab59c9fb1614c5837615172e1a2
fjlein <fjlein@gmail.com>
2025-07-07 23:43:38 +0200

    monkeypatch to always do a real uncached fetch in testing

--- tests/work/test_has_alternative_title.py
+++ tests/work/test_has_alternative_title.py
@@ -1,11 +1,14 @@
 from avefi_schema import model as efi
+from requests import Session
 
-from axiell_collections import collect_provider
+from axiell_collections import collect_provider, axiell_collections_database
 from records.record import XMLAccessor
 from records.work.has_alternative_title import has_alternative_title
 
 
-def test_has_alternative_title():
+def test_has_alternative_title(monkeypatch):
+    monkeypatch.setattr(axiell_collections_database, "session", Session())
+
     raw_xml = collect_provider.get_by_priref("200000127")
     xml = XMLAccessor(raw_xml)
 
